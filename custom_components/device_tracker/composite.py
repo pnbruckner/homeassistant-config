d57cc5bdae4eeee98d0eebb6cba493243e20c0cd
Phil Bruckner <pnbruckner@gmail.com>
2018-09-25 12:40:57 -0500

    Composite tracker: Add support for network-based trackers (#29)
    Bump version to 1.1.0.

--- custom_components/device_tracker/composite.py
+++ custom_components/device_tracker/composite.py
@@ -5,24 +5,34 @@ For more details about this platform, please refer to
 https://github.com/pnbruckner/homeassistant-config#device_trackercompositepy
 """
 
+from datetime import datetime
 import logging
 import threading
 import voluptuous as vol
 
 from homeassistant.components.device_tracker import (
-    ATTR_BATTERY, PLATFORM_SCHEMA)
+    ATTR_BATTERY, ATTR_SOURCE_TYPE, ENTITY_ID_FORMAT, PLATFORM_SCHEMA,
+    SOURCE_TYPE_GPS, SOURCE_TYPE_ROUTER)
+try:
+    from homeassistant.components.zone.zone import active_zone
+except ImportError:
+    from homeassistant.components.zone import active_zone
 from homeassistant.const import (
-    ATTR_GPS_ACCURACY, ATTR_LATITUDE, ATTR_LONGITUDE, CONF_ENTITY_ID,
-    CONF_NAME, EVENT_HOMEASSISTANT_START)
+    ATTR_GPS_ACCURACY, ATTR_LATITUDE, ATTR_LONGITUDE, ATTR_STATE,
+    CONF_ENTITY_ID, CONF_NAME, EVENT_HOMEASSISTANT_START, STATE_HOME)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import track_state_change
 from homeassistant.util import dt as dt_util
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_LAST_SEEN = 'last_seen'
+
+WARNED = 'warned'
+SOURCE_TYPE = ATTR_SOURCE_TYPE
+STATE = ATTR_STATE
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
@@ -40,7 +50,12 @@ class CompositeScanner:
         self._hass = hass
         self._see = see
         entities = config[CONF_ENTITY_ID]
-        self._entities = dict.fromkeys(entities, False)
+        self._entities = {}
+        for entity_id in entities:
+            self._entities[entity_id] = {
+                WARNED: False,
+                SOURCE_TYPE: None,
+                STATE: None}
         self._dev_id = config[CONF_NAME]
         self._lock = threading.Lock()
         self._prev_seen = None
@@ -48,10 +63,10 @@ class CompositeScanner:
         self._remove = track_state_change(
             hass, entities, self._update_info)
 
-    def _bad_entity(self, entity_id, message):
-        msg = "{} {}".format(entity_id, message)
+    def _bad_entity(self, entity_id, message, remove_now=False):
+        msg = '{} {}'.format(entity_id, message)
         # Has there already been a warning for this entity?
-        if self._entities[entity_id]:
+        if self._entities[entity_id][WARNED] or remove_now:
             _LOGGER.error(msg)
             self._remove()
             self._entities.pop(entity_id)
@@ -61,42 +76,131 @@ class CompositeScanner:
                     self._hass, self._entities.keys(), self._update_info)
         else:
             _LOGGER.warning(msg)
-            self._entities[entity_id] = True
+            self._entities[entity_id][WARNED] = True
+
+    def _good_entity(self, entity_id, source_type, state=None):
+        self._entities[entity_id].update({
+            WARNED: False,
+            SOURCE_TYPE: source_type,
+            STATE: state})
+
+    def _use_router_data(self, state):
+        if state == STATE_HOME:
+            return True
+        entities = self._entities.values()
+        if any(entity[SOURCE_TYPE] == SOURCE_TYPE_GPS
+                for entity in entities):
+            return False
+        return all(entity[STATE] != STATE_HOME
+            for entity in entities
+            if entity[SOURCE_TYPE] == SOURCE_TYPE_ROUTER)
 
     def _update_info(self, entity_id, old_state, new_state):
+        if new_state is None:
+            return
+
         with self._lock:
             # Get time device was last seen, which is the entity's last_seen
             # attribute, or if that doesn't exist, then last_updated from the
             # new state object. Make sure last_seen is timezone aware in UTC.
             # Note that dt_util.as_utc assumes naive datetime is in local
             # timezone.
-            last_seen = dt_util.as_utc(
-                new_state.attributes.get(ATTR_LAST_SEEN,
-                                         new_state.last_updated))
+            last_seen = new_state.attributes.get(ATTR_LAST_SEEN)
+            if isinstance(last_seen, datetime):
+                last_seen = dt_util.as_utc(last_seen)
+            else:
+                try:
+                    last_seen = dt_util.utc_from_timestamp(float(last_seen))
+                except (TypeError, ValueError):
+                    last_seen = new_state.last_updated
 
             # Is this newer info than last update?
             if self._prev_seen and self._prev_seen >= last_seen:
-                _LOGGER.debug("Skipping: prv({}) >= new({})".format(
+                _LOGGER.debug('Skipping: prv({}) >= new({})'.format(
                     self._prev_seen, last_seen))
                 return
 
-            # GPS coordinates and accuracy are required.
-            # Battery level is optional.
+            # Try to get GPS and battery data.
             try:
                 gps = (new_state.attributes[ATTR_LATITUDE],
                        new_state.attributes[ATTR_LONGITUDE])
             except KeyError:
-                self._bad_entity(entity_id, "missing gps attributes")
-                return
-            try:
-                gps_accuracy = new_state.attributes[ATTR_GPS_ACCURACY]
-            except KeyError:
-                self._bad_entity(entity_id, "missing gps_accuracy attribute")
-                return
+                gps = None
+            gps_accuracy = new_state.attributes.get(ATTR_GPS_ACCURACY)
             battery = new_state.attributes.get(ATTR_BATTERY)
+            # Don't use location_name unless we have to.
+            location_name = None
+
+            # What type of tracker is this?
+            source_type = new_state.attributes.get(ATTR_SOURCE_TYPE)
+
+            if source_type == SOURCE_TYPE_GPS:
+                # GPS coordinates and accuracy are required.
+                if gps is None:
+                    self._bad_entity(entity_id, 'missing gps attributes')
+                    return
+                if gps_accuracy is None:
+                    self._bad_entity(entity_id,
+                                     'missing gps_accuracy attribute')
+                    return
+                self._good_entity(entity_id, SOURCE_TYPE_GPS)
+
+            elif source_type == SOURCE_TYPE_ROUTER:
+                self._good_entity(
+                    entity_id, SOURCE_TYPE_ROUTER, new_state.state)
+                if not self._use_router_data(new_state.state):
+                    return
+
+                # Don't use new GPS data if it's not complete.
+                if gps is None or gps_accuracy is None:
+                    gps = gps_accuracy = None
+                # Get current GPS data, if any, and determine if it is in
+                # 'zone.home'.
+                cur_state = self._hass.states.get(
+                    ENTITY_ID_FORMAT.format(self._dev_id))
+                try:
+                    cur_lat = cur_state.attributes[ATTR_LATITUDE]
+                    cur_lon = cur_state.attributes[ATTR_LONGITUDE]
+                    cur_acc = cur_state.attributes[ATTR_GPS_ACCURACY]
+                    cur_gps_is_home = (
+                        active_zone(self._hass, cur_lat, cur_lon, cur_acc)
+                        .entity_id == 'zone.home')
+                except (AttributeError, KeyError):
+                    cur_gps_is_home = False
+
+                # It's important, for this composite tracker, to avoid the
+                # component level code's "stale processing." This can be done
+                # one of two ways: 1) provide GPS data w/ source_type of gps,
+                # or 2) provide a location_name (that will be used as the new
+                # state.)
+
+                # If router entity's state is 'home' and current GPS data from
+                # composite entity is available and is in 'zone.home',
+                # use it and make source_type gps.
+                if new_state.state == STATE_HOME and cur_gps_is_home:
+                    gps = cur_lat, cur_lon
+                    gps_accuracy = cur_acc
+                    source_type = SOURCE_TYPE_GPS
+                # Otherwise, if new GPS data is valid (which is unlikely if
+                # new state is not 'home'),
+                # use it and make source_type gps.
+                elif gps:
+                    source_type = SOURCE_TYPE_GPS
+                # Otherwise, don't use any GPS data, but set location_name to
+                # new state.
+                else:
+                    location_name = new_state.state
+
+            else:
+                self._bad_entity(
+                    entity_id,
+                    'unsupported source_type: {}'.format(source_type),
+                    remove_now=True)
+                return
 
             attrs = {ATTR_LAST_SEEN: last_seen.replace(microsecond=0)}
-            self._see(dev_id=self._dev_id, gps=gps, gps_accuracy=gps_accuracy,
-                battery=battery, attributes=attrs)
+            self._see(dev_id=self._dev_id, location_name=location_name,
+                gps=gps, gps_accuracy=gps_accuracy, battery=battery,
+                attributes=attrs, source_type=source_type)
 
             self._prev_seen = last_seen
--- docs/composite.md
+++ docs/composite.md
@@ -1,5 +1,7 @@
 # Composite Device Tracker
-This platform creates a composite device tracker from one or more other device trackers. It will update whenever one of the watched entities updates, taking the GPS and last_seen/last_updated (and possibly battery) data from the changing entity. The result can be a more accurate and up-to-date device tracker if the "input" device tracker's update irregularly.
+This platform creates a composite device tracker from one or more other device trackers. It will update whenever one of the watched entities updates, taking the last_seen/last_updated (and possibly GPS and battery) data from the changing entity. The result can be a more accurate and up-to-date device tracker if the "input" device tracker's update irregularly.
+
+Currently device_tracker's with a source_type of gps or router are supported.
 ## Installation
 See [Installing and Updating](https://github.com/pnbruckner/homeassistant-config/blob/master/docs/custom_updater.md) to use Custom Updater.
 
@@ -21,16 +23,23 @@ device_tracker:
 ## Configuration variables
 - **name**: Object ID of composite device: `device_tracker.NAME`
 - **entity_id**: Watched device tracker devices.
-## Watched device requirements
-Watched devices must have, at a minimum, the following attributes: `latitude`, `longitude` and `gps_accuracy`. If they don't they will not be used.
+## Watched device notes
+Watched GPS-based devices must have, at a minimum, the following attributes: `latitude`, `longitude` and `gps_accuracy`. If they don't they will not be used.
+
+For watched network-based devices, which states are used and whether any GPS data (if present) is used depends on several factors. E.g., if GPS-based devices are in use then the 'not_home' state of network-based devices will be ignored. If only network-based devices are in use, then the composite device will be 'home' if any of the watched devices are 'home', and will be 'not_home' only when _all_ the watched devices are 'not_home'.
 
 If a watched device has a `last_seen` attribute, that will be used in the composite device. If not, then `last_updated` from the entity's state will be used instead.
 
 If a watched device has a `battery` attribute, that will be used to update the composite device.
 ## known_devices.yaml
-The watched devices, and the composite device, should all have `track` set to `true`. It's recommended, as well, to set `hide_if_away` to `true` for the watched devices (but leave it set to `false` for the composite device.) This way the map will only show the composite device (when it is out of the home zone.)
+The watched devices, and the composite device, should all have `track` set to `true`.
+
+It's recommended, as well, to set `hide_if_away` to `true` for the watched devices (but leave it set to `false` for the composite device.) This way the map will only show the composite device (when it is out of the home zone.)
+
+Lastly, it is also recommended to _not_ use the native merge feature of the device tracker component (i.e., do not add the MAC address from network-based trackers to a GPS-based tracker. See more details in the [Device Tracker doc page](https://www.home-assistant.io/components/device_tracker/#using-gps-device-trackers-with-local-network-device-trackers).)
 ## Release Notes
 Date | Version | Notes
 -|:-:|-
 20180907 | [1.0.0](https://github.com/pnbruckner/homeassistant-config/blob/d767bcce0fdff0c9298dc7a010d27af88817eac2/custom_components/device_tracker/composite.py) | Initial support for Custom Updater.
 20180920 | [1.0.1](https://github.com/pnbruckner/homeassistant-config/blob/959a691afb5e2b98e946591b9ff58291f104a74a/custom_components/device_tracker/composite.py) | Add thread locking to protect against multiple entities updating too close together.
+20180925 | [1.1.0](https://github.com/pnbruckner/homeassistant-config/blob/aa3b39c136f505cad8f64507362c0700ea957df6/custom_components/device_tracker/composite.py) | Add support for network-based device trackers.
