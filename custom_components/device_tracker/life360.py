50a070f87ea5d3c6f2cc3ebd7c8de82953ea32c1
Phil Bruckner <pnbruckner@gmail.com>
2018-11-02 13:22:21 -0500

    Update life360.py
    Remove beta suffix.

--- custom_components/device_tracker/life360.py
+++ custom_components/device_tracker/life360.py
@@ -31,7 +31,7 @@ from homeassistant.util.distance import convert
 import homeassistant.util.dt as dt_util
 
 
-__version__ = '2.0.0b3'
+__version__ = '2.0.0'
 
 _LOGGER = logging.getLogger(__name__)
 
