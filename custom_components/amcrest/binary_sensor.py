a18bf44d05597c59cf52cee5675b915260535ab8
Phil Bruckner <pnbruckner@gmail.com>
2019-02-26 14:58:31 -0600

    Update binary_sensor.py

--- custom_components/amcrest/binary_sensor.py
+++ custom_components/amcrest/binary_sensor.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import logging
 from requests.exceptions import RequestException
 
-from . import DATA_AMCREST, DATA_AMCREST_LOCK, LOCK_TIMEOUT, BINARY_SENSORS
+from . import DATA_AMCREST, BINARY_SENSORS
 from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.const import CONF_NAME, CONF_BINARY_SENSORS
 
@@ -24,23 +24,21 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     device_name = discovery_info[CONF_NAME]
     binary_sensors = discovery_info[CONF_BINARY_SENSORS]
     amcrest = hass.data[DATA_AMCREST][device_name]
-    lock = hass.data[DATA_AMCREST_LOCK][device_name]
 
     amcrest_binary_sensors = []
     for sensor_type in binary_sensors:
         amcrest_binary_sensors.append(
-            AmcrestBinarySensor(amcrest.name, amcrest.device, sensor_type, lock))
+            AmcrestBinarySensor(amcrest.name, amcrest.device, sensor_type))
 
     async_add_devices(amcrest_binary_sensors, True)
     return True
 
 class AmcrestBinarySensor(BinarySensorDevice):
 
-    def __init__(self, name, camera, sensor_type, lock):
+    def __init__(self, name, camera, sensor_type):
         self._name = '{} {}'.format(name, BINARY_SENSORS.get(sensor_type))
         self._camera = camera
         self._sensor_type = sensor_type
-        self._lock = lock
         self._state = None
 
     @property
@@ -58,12 +56,8 @@ class AmcrestBinarySensor(BinarySensorDevice):
     def update(self):
         _LOGGER.debug('Pulling data from {} binary sensor.'.format(self._name))
 
-        if self._lock.acquire(timeout=LOCK_TIMEOUT):
+        if self._sensor_type == 'motion_detected':
             try:
-                if self._sensor_type == 'motion_detected':
-                    try:
-                        self._state = self._camera.is_motion_detected
-                    except RequestException as exc:
-                        _LOGGER.error('{}: {}'.format(exc.__class__.__name__, str(exc)))
-            finally:
-                self._lock.release()
+                self._state = self._camera.is_motion_detected
+            except RequestException as exc:
+                _LOGGER.error('{}: {}'.format(exc.__class__.__name__, str(exc)))
