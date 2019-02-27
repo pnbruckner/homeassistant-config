6ae2600eb13c0b7fdeb972e962cbd49ff9711427
Phil Bruckner <pnbruckner@gmail.com>
2019-02-27 16:31:19 -0600

    Update state_updates.py
    Accept multiple entity_id's, each with its own list of attributes. When more than one entity_id merge attributes into a single column.

--- tools/state_updates.py
+++ tools/state_updates.py
@@ -7,59 +7,123 @@ import sys
 
 try:
     filename = sys.argv[1]
-    entity_id = sys.argv[2]
+    if '.' not in sys.argv[2]:
+        raise ValueError
 except:
-    print('Usage: python3 {} filename entity_id [attribute...]'.format(sys.argv[0]))
+    print('Usage: python3 {} filename (entity_id [attribute...])...'.format(sys.argv[0]))
     sys.exit(1)
-attrs = sys.argv[3:]
 
-new_state_format = r'([0-9-]+ [0-9:]+).*new_state=<state {}=([^;]+); (.*) @ ([0-9-:.T]+)>.*'
+attrs = {}
+entity_id = None
+for arg in sys.argv[2:]:
+    if '.' in arg:
+        if entity_id is not None:
+            attrs[entity_id] = entity_attrs
+        entity_id = arg
+        entity_attrs = []
+    else:
+        entity_attrs.append(arg)
+attrs[entity_id] = entity_attrs
 
-states = []
+haevent = re.compile(r'([0-9-]+ [0-9:]+).*homeassistant_(start|close|stop).*')
+new_state = re.compile(
+    r'([0-9-]+ [0-9:]+).*new_state=<state ([^=]+)=([^;]+); (.*) @ ([0-9-:.T]+)>.*')
+
+ent_hdr = 'entity_id'
+max_ent = len(ent_hdr)
 ts_hdr = 'log time'
 max_ts = len(ts_hdr)
 lc_hdr = 'last_changed'
 max_lc = len(lc_hdr)
 state_hdr = 'state'
 max_state = len(state_hdr)
-max_attr = {}
-for attr in attrs:
-    max_attr[attr] = len(attr)
+if len(attrs) == 1:
+    max_attr = {}
+    for attr in entity_attrs:
+        max_attr[attr] = len(attr)
+else:
+    attr_hdr = 'attributes'
 
+HAEVENT = 'Home Assistant'
+HAFMT = ' {} {{}} '.format(HAEVENT)
+
+states = []
 with open(filename) as f:
-    new_state = new_state_format.format(entity_id.replace('.', '\.'))
-    p = re.compile(new_state)
     for line in f:
-        m = p.match(line)
+        m = haevent.match(line)
         if m:
             ts = m.group(1)
             max_ts = max(max_ts, len(ts))
-            last_changed = m.group(4)
+            last_changed = HAFMT.format(m.group(2).title())
             max_lc = max(max_lc, len(last_changed))
-            state = m.group(2)
+            states.append((None, ts, last_changed, None, None))
+            continue
+        m = new_state.match(line)
+        if m and m.group(2) in attrs:
+            entity_id = m.group(2)
+            max_ent = max(max_ent, len(entity_id))
+            ts = m.group(1)
+            max_ts = max(max_ts, len(ts))
+            last_changed = m.group(5)
+            max_lc = max(max_lc, len(last_changed))
+            state = m.group(3)
             max_state = max(max_state, len(state))
-            s = m.group(3)
+            s = m.group(4)
             _attrs = OrderedDict()
-            for attr in attrs:
+            for attr in attrs[entity_id]:
                 try:
                     start = s.index(attr+'=')+len(attr)+1
                     _attr = s[start:s.rfind(', ', start, s.find('=', start))]
                 except:
                     _attr = '???'
                 _attrs[attr] = _attr
-                max_attr[attr] = max(max_attr[attr], len(_attr))
-            states.append((ts, last_changed, state, _attrs))
+            if len(attrs) == 1:
+                for attr in entity_attrs:
+                    max_attr[attr] = max(max_attr[attr], len(_attrs[attr]))
+            states.append((entity_id, ts, last_changed, state, _attrs))
 
+if len(attrs) > 1:
+    print('{:{}} | '.format(ent_hdr, max_ent), end='')
 print('{:{}} | {:{}} | {:{}}'.format(ts_hdr, max_ts, lc_hdr, max_lc, state_hdr, max_state), end='')
-for attr in attrs:
-    print(' | {:{}}'.format(attr, max_attr[attr]), end='')
+if len(attrs) == 1:
+    for attr in entity_attrs:
+        print(' | {:{}}'.format(attr, max_attr[attr]), end='')
+else:
+    print(' | {}'.format(attr_hdr), end='')
 print('')
+
+if len(attrs) > 1:
+    print('-'*max_ent, end='-|-')
 print('-'*max_ts, '-'*max_lc, '-'*max_state, sep='-|-', end='')
-for attr in attrs:
-    print('', '-'*max_attr[attr], sep='-|-', end='')
+if len(attrs) == 1:
+    for attr in entity_attrs:
+        print('', '-'*max_attr[attr], sep='-|-', end='')
+else:
+    print('-|-', end='')
+    print('-'*len(attr_hdr), end='')
 print('')
-for ts, last_changed, state, _attrs in states:
-    print('{:{}} | {:{}} | {:{}}'.format(ts, max_ts, last_changed, max_lc, state, max_state), end='')
-    for k,v in _attrs.items():
-        print(' | {:{}}'.format(v, max_attr[k]), end='')
+
+prev_entity_id = None
+for entity_id, ts, last_changed, state, _attrs in states:
+    if HAEVENT in last_changed:
+        entity_id = '='*max_ent
+        last_changed = '{:=^{}}'.format(last_changed, max_lc)
+        state = '='*max_state
+        if len(attrs) == 1:
+            _attrs = OrderedDict()
+            for attr in entity_attrs:
+                _attrs[attr] = '='*max_attr[attr]
+        else:
+            _attrs = {'=': '='*(len(attr_hdr)-2)}
+    if len(attrs) > 1:
+        print('{:{}} | '.format('' if entity_id == prev_entity_id and HAEVENT not in last_changed else entity_id, max_ent), end='')
+    prev_entity_id = entity_id
+    print('{:{}} | {:{}} | {:{}}'.format(ts, max_ts, last_changed , max_lc, state , max_state), end='')
+    if len(attrs) == 1:
+        for k,v in _attrs.items():
+            print(' | {:{}}'.format(v if HAEVENT not in last_changed else '='*max_attr[k], max_attr[k]), end='')
+    else:
+        print(' |', end='')
+        for k,v in _attrs.items():
+            print(' {}={}'.format(k, v), end='')
     print('')
