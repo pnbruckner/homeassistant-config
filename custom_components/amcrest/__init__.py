dc4f1d473430860e02dfa4ddf51038c131095d8a
Phil Bruckner <pnbruckner@gmail.com>
2019-04-02 16:15:38 -0500

    Update __init__.py

--- custom_components/amcrest/__init__.py
+++ custom_components/amcrest/__init__.py
@@ -13,7 +13,7 @@ from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
 
 
-__version__ = '1.2.2'
+__version__ = '1.3.0'
 
 REQUIREMENTS = ['amcrest==1.3.0']
 DEPENDENCIES = ['ffmpeg']
