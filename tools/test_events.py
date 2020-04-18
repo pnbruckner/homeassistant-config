0682cd8b69e92177942cbc3a9deb48e231eff68b
Phil <pnbruckner@gmail.com>
2020-04-18 11:03:53 -0500

    test_events.py: Add logging

--- tools/test_events.py
+++ tools/test_events.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import logging
 import sys
 
 from amcrest import Http
@@ -14,25 +15,53 @@ def lines(ret):
 
 
 def main():
-    if len(sys.argv) != 5:
-        print(f'{sys.argv[0]} host port user password')
+    if len(sys.argv) not in (5, 6):
+        print(f'{sys.argv[0]} host port user password [log]')
         sys.exit(1)
 
     host = sys.argv[1]
     port = sys.argv[2]
     user = sys.argv[3]
     pswd = sys.argv[4]
+    log = len(sys.argv) == 6
+    if log:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(levelname)s: %(name)s: %(message)s",
+        )
+        urllib3_logger = logging.getLogger("urllib3.connectionpool")
+        if not any(isinstance(x, NoHeaderErrorFilter) for x in urllib3_logger.filters):
+            urllib3_logger.addFilter(NoHeaderErrorFilter())
 
     cam = Http(host, port, user, pswd, retries_connection=1, timeout_protocol=3.05)
 
-    print(cam.device_type)
-    print(*cam.software_information)
-    print(cam.version_http_api)
+    dev_type = cam.device_type
+    if log:
+        print()
+    print(dev_type.strip())
+    if log:
+        print()
+
+    sw_info = cam.software_information
+    if log:
+        print()
+    for info in sw_info:
+        print(info.strip())
+    if log:
+        print()
+
+    ver_http = cam.version_http_api
+    if log:
+        print()
+    print(ver_http.strip())
+    if log:
+        print()
 
     ret = cam.command(
         'eventManager.cgi?action=attach&codes=[VideoMotion]',
         timeout_cmd=(3.05, None), stream=True)
     ret.encoding = 'utf-8'
+    print()
 
     try:
         for line in lines(ret):
@@ -46,6 +75,18 @@ def main():
     except KeyboardInterrupt:
         ret.close()
         print(' Done!')
+
+
+class NoHeaderErrorFilter(logging.Filter):
+    """
+    Filter out urllib3 Header Parsing Errors due to a urllib3 bug.
+
+    See https://github.com/urllib3/urllib3/issues/800
+    """
+
+    def filter(self, record):
+        """Filter out Header Parsing Errors."""
+        return "Failed to parse headers" not in record.getMessage()
 
 
 if __name__ == '__main__':
